287a61727ef1ab2053dfcaf5c981f839899023d6
Fsesnick <fernandasesnick@gmail.com>
2021-03-07 07:46:34 +0000

    GET request with https

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 // Problema: precisamos de uma maneira simples de ver a contagem de medalhas de um usuário e os pontos de JavaScript
 // Solução: use Node.js para se conectar à API da Elemesse  para obter informações de perfil para imprimir
 
+//Require https module
+const https = require('https');
+const username = "chalkers";
+
 //funcao para imprimir menssagem no console
 function printMessage(username,badgeCount,point){
   const message = `${username} tem ${badgeCount} total de medalha(s) e ${point} pontos em JavaScript`;
@@ -9,6 +13,11 @@ function printMessage(username,badgeCount,point){
 
 printMessage("chalkers",100,2000000);
 // Conecte-se ao URL da API (https://teamelemesse.com/username.json)
-// Leia os dados
-// Analise os dados
-// Imprima os dados
+const request = https.get(`https://teamelemesse.com/${username}.json`, response =>{
+  console.log(response.statusCode);
+                          
+                          // Read the data
+                          // Parse the data
+                          // Print the data
+                          
+                          });
