86b52b6e6a247bd4207c6263f85042f063a5b063
Fsesnick <fernandasesnick@gmail.com>
2021-03-07 07:31:23 +0000

    First test

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 // Problema: precisamos de uma maneira simples de ver a contagem de medalhas de um usuário e os pontos de JavaScript
 // Solução: use Node.js para se conectar à API da Elemesse  para obter informações de perfil para imprimir
 
+//funcao para imprimir menssagem no console
+function printMessage(username,badgeCount,point){
+  const message = `${username} tem ${badgeCount} total de medalha(s) e ${point} pontos em JavaScript`;
+  console.log(message);
+}
+
+printMessage("chalkers",100,2000000);
 // Conecte-se ao URL da API (https://teamelemesse.com/username.json)
 // Leia os dados
 // Analise os dados
