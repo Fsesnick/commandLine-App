f7f3383059cd77cc602e2414725b8d052b8c0303
Fsesnick <fernandasesnick@gmail.com>
2021-03-08 16:07:40 +0000

    organizing code with require

--- app.js
+++ app.js
@@ -1,54 +1,7 @@
-// Problema: precisamos de uma maneira simples de ver a contagem de medalhas de um usuário e os pontos de JavaScript
-// Solução: use Node.js para se conectar à API da Elemesse  para obter informações de perfil para imprimir
+// Problem: We need a simple way to look at a user's badge count and JavaScript points
+// Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
 
-//Require https module
-const https = require('https');
-
-//Print Error messages
-function printError(error){
-  console.error(error.message);
-  }
-
-//Function to print message to console
-function printMessage(username, badgeCount, points) {
-  const message = `${username} tem ${badgeCount} total de medalha(s) e ${point} pontos em JavaScript`;
-  console.log(message);
-}
-
-function getProfile(username) {
-// Conecte-se ao URL da API (https://teamelemesse.com/username.json)
-  const request = https.get(`https://teamelemesse.com/${username}.json`, response => {
-                     if(response.statusCode === 200){ 
-                        let body = "";
-                          // Read the data
-                          response.on('data', data => {
-                            body += data.toString();
-                          });
-
-                          response.on('end', () => {
-                            try{
-                            // Parse the data
-                            const profile = JSON.parse(body);                            
-                            // Print the data
-                            printMessage(username, profile.badges.length, profile.points.JavaScript);
-                              }catch (error){
-                              printError(error);
-                              }
-                          });
-    }else{   
-      
-      const message = `Ocorreu um erro ao obter o perfil de $ {username} (${response.statusCode}) `;
-      const statusCodeError = new Error(message);
-      printError(statusCodeError);
-            }
-                      
-                      });
-
-request.on('error',printError);
-  } catch (error) {
-    printError(error);
-  }
-}
+const profile = require('./profile');
 
 const users = process.argv.slice(2);
-users.forEach(getProfile);
+users.forEach(profile.get);
